ca8448a33e08fd87fab2ca0d1fd599278369fc4d
Michal Jazwinski <mail@mihow.pl>
2022-09-08 15:10:30 +0200

    added extension method to WhenNotificationEx method

---
+++ PLCObserverMinimalExample/Extensions.cs
@@ -0,0 +1,92 @@
+﻿using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Runtime.CompilerServices;
+using TwinCAT.Ads.Internal;
+using TwinCAT.Ads.Tracing;
+using TwinCAT.Ads;
+using TwinCAT.TypeSystem;
+using TwinCAT.Ads.Reactive;
+
+namespace PLCObserverMinimalExample
+{
+    public static class Extensions
+    {
+        public static Dictionary<string, uint> Handles { get; private set; } = new();
+
+        public static IObservable<ValueNotification> WhenNotificationWithHandle(this IAdsConnection connection, IList<AnySymbolSpecifier> symbols, NotificationSettings settings)
+        {
+            IAdsConnection connection2 = connection;
+            IList<AnySymbolSpecifier> symbols2 = symbols;
+            NotificationSettings settings2 = settings;
+            if (connection2 == null)
+            {
+                throw new ArgumentNullException("connection");
+            }
+
+            if (symbols2 == null)
+            {
+                throw new ArgumentNullException("symbols");
+            }
+
+            if (symbols2.Count == 0)
+            {
+                throw new ArgumentOutOfRangeException("symbols", "Symbol list is empty!");
+            }
+
+            IDisposableHandleBag<AnySymbolSpecifier> bag = null;
+            EventLoopScheduler scheduler = new EventLoopScheduler();
+            IObservable<int> whenSymbolChangeObserver = connection2.WhenSymbolVersionChanges(scheduler);
+            IDisposable whenSymbolChanges = null;
+            Action<EventHandler<AdsNotificationExEventArgs>> addHandler = delegate (EventHandler<AdsNotificationExEventArgs> h)
+            {
+                connection2.AdsNotificationEx += h;
+                bag = ((IAdsHandleCacheProvider)connection2).CreateNotificationExHandleBag(symbols2, relaxSubErrors: false, settings2, null);
+                bag.CreateHandles();
+
+                // Collect Handles
+                Handles.Clear();
+                foreach (var item in bag.SourceResultHandles)
+                    Handles.Add(item.source.InstancePath, item.result.Handle);
+
+                whenSymbolChanges = whenSymbolChangeObserver.Subscribe((Action<int>)delegate
+                {
+                    bag.CreateHandles();
+                    Handles.Clear();
+                    foreach (var item in bag.SourceResultHandles)
+                        Handles.Add(item.source.InstancePath, item.result.Handle);
+
+                }, (Action<Exception>)delegate
+                {
+                    TcTraceSource traceAds = AdsModule.TraceAds;
+                    DefaultInterpolatedStringHandler defaultInterpolatedStringHandler = new DefaultInterpolatedStringHandler(101, 1);
+                    defaultInterpolatedStringHandler.AppendLiteral("The AdsServer '");
+                    defaultInterpolatedStringHandler.AppendFormatted(connection2.Address);
+                    defaultInterpolatedStringHandler.AppendLiteral("' doesn't support SymbolVersionChanged Notifications! Handle recreation is not active!");
+                    traceAds.TraceInformation(defaultInterpolatedStringHandler.ToStringAndClear());
+                });
+            };
+            Action<EventHandler<AdsNotificationExEventArgs>> removeHandler = delegate (EventHandler<AdsNotificationExEventArgs> h)
+            {
+                if (whenSymbolChanges != null)
+                {
+                    whenSymbolChanges.Dispose();
+                }
+
+                scheduler.Dispose();
+                if (bag != null)
+                {
+                    bag.Dispose();
+                    bag = null;
+
+                    Handles.Clear();
+                }
+
+                connection2.AdsNotificationEx -= h;
+            };
+
+            return from ev in Observable.FromEventPattern<EventHandler<AdsNotificationExEventArgs>, AdsNotificationExEventArgs>(addHandler, removeHandler)
+                   where bag.Contains(ev.EventArgs.Handle)
+                   select new ValueNotification(ev.EventArgs, ev.EventArgs.Value);
+        }
+    }
+}
--- PLCObserverMinimalExample/Program.cs
+++ PLCObserverMinimalExample/Program.cs
@@ -3,6 +3,7 @@ using TwinCAT.Ads;
 using TwinCAT.TypeSystem;
 using System.Reactive;
 using TwinCAT.Ads.Reactive;
+using PLCObserverMinimalExample;
 
 IDisposable _subscription = null;
 AdsClient _adsClient = new();
@@ -15,7 +16,7 @@ if (plcState == AdsState.Run)
     // Attach notification
     var valueNotificationVariables = new List<(string, Type)>();
     valueNotificationVariables.Add((".bPulse05s", typeof(bool)));
-    valueNotificationVariables.Add((".bPulse1s", typeof(int)));
+    valueNotificationVariables.Add((".bPulse1s", typeof(bool)));
     AttachNotification(valueNotificationVariables);
 
     Console.WriteLine("Press any button to close...");
@@ -28,21 +29,19 @@ void AttachNotification(IEnumerable<(string key, Type type)> Symbols)
 {
     var observerSymbolNotification = Observer.Create<ValueNotification>(val =>
     {
-        var handle = val.Handle;
-        if (val.UserData is object[] objects)
+        // Identify the changed Variable
+        if (Extensions.Handles.Any(x => x.Value == val.Handle))
         {
-            // Based on Handle get value from userData
-            ;
+            var tag = Extensions.Handles.First(x => x.Value == val.Handle).Key;
 
-            //string tag = objects[handle - 2].ToString();
-
-            //if (!_values.Any(x => x.Key == tag))
-            //    _values.Add(new SymbolModel { Key = tag, Value = val.Value });
-            //else
-            //{
-            //    var symbol = _values.First(x => x.Key == tag);
-            //    symbol.Value = val.Value;
-            //}
+            // Collect to dictionary
+            if (!_values.Any(x => x.Key == tag))
+                _values.Add(new SymbolModel { Key = tag, Value = val.Value });
+            else
+            {
+                var symbol = _values.First(x => x.Key == tag);
+                symbol.Value = val.Value;
+            }
         }
     });
 
@@ -56,7 +55,7 @@ void AttachNotification(IEnumerable<(string key, Type type)> Symbols)
             list.Add(new AnySymbolSpecifier(key, new AnyTypeSpecifier(type)));
             userData.Add(key);
         }
-        _subscription = _adsClient.WhenNotificationEx(list, NotificationSettings.ImmediatelyOnChange, userData.ToArray())
+        _subscription = _adsClient.WhenNotificationWithHandle(list, NotificationSettings.ImmediatelyOnChange)
                                               .Subscribe(observerSymbolNotification);
     }
 }
